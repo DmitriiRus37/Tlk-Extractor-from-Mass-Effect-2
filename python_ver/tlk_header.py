1c92b33289bd8d3998012d85045fa53486759512
d.gurylev <d.gurylev@tdm-tech.ru>
2023-04-21 16:10:45 +0700

    minor renames [2]

--- python_ver/tlk_header.py
+++ python_ver/tlk_header.py
@@ -4,10 +4,10 @@ import tlk_file
 
 class TlkHeader:
     def __init__(self, in_stream):
-        self.magic = input_stream.read_int_32(in_stream)
-        self.ver = input_stream.read_int_32(in_stream)
-        self.min_ver = input_stream.read_int_32(in_stream)
-        self.entry_1_count = input_stream.read_int_32(in_stream)
-        self.entry_2_count = input_stream.read_int_32(in_stream)
+        self.magic = input_stream.read_int_32(in_stream)  # TODO ???
+        self.ver = input_stream.read_int_32(in_stream)  # TODO ???
+        self.min_ver = input_stream.read_int_32(in_stream)  # TODO ???
+        self.entry_1_count = input_stream.read_int_32(in_stream)  # TODO ???
+        self.entry_2_count = input_stream.read_int_32(in_stream)  # TODO ???
         self.tree_nodes_count = input_stream.read_int_32(in_stream)  # count of unique symbols
-        self.data_len = input_stream.read_int_32(in_stream)
+        self.data_len = input_stream.read_int_32(in_stream)  # TODO bytes, bits or symbols???
